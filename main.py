37be2d740f249de5c86ca5c7dec552732c1d3e22
honulu <honuluhfl9@gmail.com>
2019-05-16 23:10:17 +0800

    add cycle

--- main.py
+++ main.py
@@ -2,28 +2,60 @@ import requests
 import re
 from bs4 import BeautifulSoup
 import html
+
+
 def getSour(url):
     r = requests.get(url, 'html.parsers')
     r.encoding = r.apparent_encoding
     r = html.unescape(r.text)
     return r
+
+
 def saveFile(d):
-    r = open("C:\\Users\\honulu\\Desktop\\code\\python\\tsgBooks\\data\\test.html","w",encoding="utf-8")
-    r.
+    r = open("C:\\Users\\honulu\\Desktop\\code\\python\\tsgBooks\\data\\test.html",
+             "w", encoding="utf-8")
     r.write(d)
     r.close()
-def analyWeb(soup):
+
+
+def analyWeb(r, test):
+    soup = BeautifulSoup(r, 'html.parser')
     k = soup.findAll("div", "list_books")
     for i in k:
         # books's name
-        print( re.sub(r'\d*\. ',"",i.h3.get_text()))
+        bookNmae = re.sub(r'\d*\. ', "", i.h3.get_text())
         # book's website
-        print(re.sub(r"\D", "", i.p.span["id"]))
+        bookWebsite = re.sub(r"\D", "", i.p.span["id"])
         # book's publisher
-        print(re.sub(r"\s", "", i.p.get_text()))
+        bookPublisher = re.sub(r"\s", "", i.p.get_text())
+        books = bookNmae + bookPublisher + bookWebsite
+        print(books)
+        test += 1
+        print("***********", test)
+    return test
+
+
+def fUrls(url):
+    orign = url
+    r = requests.get(orign)
+    soup = BeautifulSoup(r.text,'html.parser')
+    k = soup.findAll("option")
+    n = 0
+    for i in k:
+        n += 1
+    return n
+
+
 def main():
-    url = "http://172.16.47.83/newbook/newbook_cls_book.php?back_days=15&loca_code=ALL&cls=TP&s_doctype=ALL&clsname=自动化技术、计算机技术&page=2"
-    k = getSour(url)
-    saveFile(k)
+    url = "http://172.16.47.83/newbook/newbook_cls_book.php?back_days=15&loca_code=ALL&cls=TP&s_doctype=ALL&clsname=自动化技术、计算机技术"
+    n = fUrls(url)
+    print("all numbers:", n)
+    test = 0
+    for i in range(n):
+        urls = url + str(i)
+        r = getSour(urls)
+        test = analyWeb(r, test)
+    print("all books are", test)
+
 
 main()
