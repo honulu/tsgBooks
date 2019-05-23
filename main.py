0173d60f48f67c307cf52ffaa9c732a89cc8b382
honulu <honuluhfl9@gmail.com>
2019-05-23 17:55:12 +0800

    some

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ import requests
 import re
 from bs4 import BeautifulSoup
 import html
+import sqlite3
 
 
+# Requests the URL and return text
 def getSour(url):
     r = requests.get(url, 'html.parsers')
     r.encoding = r.apparent_encoding
     r = html.unescape(r.text)
     return r
 
-
+# Save the file to test.html
 def saveFile(d):
     r = open("C:\\Users\\honulu\\Desktop\\code\\python\\tsgBooks\\data\\test.html",
              "w", encoding="utf-8")
@@ -23,18 +25,29 @@ def analyWeb(r, test):
     k = soup.findAll("div", "list_books")
     for i in k:
         # books's name
-        bookNmae = re.sub(r'\d*\. ', "", i.h3.get_text())
+        bookName = re.sub(r'\d*\. ', "", i.h3.get_text())
         # book's website
         bookWebsite = re.sub(r"\D", "", i.p.span["id"])
         # book's publisher
         bookPublisher = re.sub(r"\s", "", i.p.get_text())
-        books = bookNmae + bookPublisher + bookWebsite
+        books = bookName + bookPublisher + bookWebsite
         print(books)
         test += 1
         print("***********", test)
     return test
-
-
+def SaveToSql():
+    conn = sqlite3.connect("tsgBooks.db")
+    c = conn.cursor()
+    c.executemany('''INSERT INTO stocks
+                    (bookName, bookWebsite, bookPublisher)''')
+    conn.commit()
+    conn.close()
+def NewDb():
+    conn = sqlite3.connect("tsgBooks.db")
+    c = conn.cursor()
+    c.execute('''CREATE TABLE
+                (bookName, bookWebsite, bookPublisher)''')
+# Get the pages of books
 def fUrls(url):
     orign = url
     r = requests.get(orign)
@@ -51,8 +64,9 @@ def main():
     n = fUrls(url)
     print("all numbers:", n)
     test = 0
-    for i in range(n):
-        urls = url + str(i)
+    for i in range(1, n+1):
+        urls = url + "&page="+ str(i)
+        print("NEXT WE WILL %s", urls)
         r = getSour(urls)
         test = analyWeb(r, test)
     print("all books are", test)
